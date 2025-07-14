77f8027585ed079515899c9bf4336f8d3a140f5f
Estela Medeiros <139095335+xeanm28x@users.noreply.github.com>
2025-07-14 15:11:30 -0300

    mais configurações de banco e editor ts (por causa das models)

--- start/env.ts
+++ start/env.ts
@@ -25,4 +25,5 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_NAME: Env.schema.string(),
+  DB_DATABASE: Env.schema.string(),
 })
